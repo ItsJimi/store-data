0d4bdb1762eb531763ff478d6b99417c80087398
ItsJimi <jimi.maiquez@gmail.com>
2017-04-03 23:08:14 +0200

    Get/Set, init and backup tests

--- examples/basic.js
+++ examples/basic.js
@@ -2,5 +2,12 @@ var store = require('../lib');
 
 store.init({
   directory: 'documents',
-  documents: ['users', 'tests', 'cars']
+  documents: ['users', 'tests', 'cars', 'blabla']
 });
+
+console.log(store.get('users', 'abc123'));
+store.set('users', 'abc123', {
+  name: 'Jimi',
+  age: 19
+});
+console.log(store.get('users', 'abc123'));
