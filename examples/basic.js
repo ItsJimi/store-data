1d837eeb304b82a78e3dded457afbc6d4fbd7c19
ItsJimi <jimi.maiquez@gmail.com>
2017-04-08 08:56:02 +0200

    First try backup system

--- examples/basic.js
+++ examples/basic.js
@@ -5,9 +5,11 @@ store.init({
   documents: ['users', 'tests', 'cars', 'blabla']
 });
 
-console.log(store.get('users', 'abc123'));
-store.set('users', 'abc123', {
-  name: 'Jimi',
-  age: 19
-});
-console.log(store.get('users', 'abc123'));
+// console.log(store.get('users', 'abc123'));
+// store.set('users', 'abc123', {
+//   name: 'Jimi',
+//   age: 19
+// });
+// console.log(store.get('users', 'abc123'));
+
+// console.log(store.keys());
