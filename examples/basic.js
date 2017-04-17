d7fa0f6dd94a5b6dad458da907ff8d9fba7576b8
ItsJimi <jimi.maiquez@gmail.com>
2017-04-17 17:35:19 +0200

    Fix #5, refactor without Map

--- examples/basic.js
+++ examples/basic.js
@@ -1,15 +1,15 @@
-var store = require('../lib');
+var store = require('../lib')
 
 store.init({
   directory: 'documents',
   documents: ['users', 'tests', 'cars', 'blabla']
-});
+})
 
-// console.log(store.get('users', 'abc123'));
-// store.set('users', 'abc123', {
-//   name: 'Jimi',
-//   age: 19
-// });
-// console.log(store.get('users', 'abc123'));
+console.log(store.get('users', 'abc123'))
+store.set('users', 'abc123', {
+  name: 'Jimi',
+  age: 19
+})
+console.log(store.get('users', 'abc123'))
 
-// console.log(store.keys());
+// console.log(store.keys('users'))
