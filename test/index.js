e76b5efafad0b7bc95e870bbade21607e18ec750
ItsJimi <jimi.maiquez@gmail.com>
2017-04-19 23:27:19 +0200

    new tests

--- test/index.js
+++ test/index.js
@@ -4,25 +4,63 @@ var store = require('../lib')
 /* global describe it */
 
 describe('store-data', () => {
-  describe('Basic', () => {
+  describe('In Memory', () => {
     it('init', () => {
       var init = store.init({
-        directory: 'documents',
         documents: ['users'],
         save: false
       })
       assert.equal(init, true)
     })
     it('get', () => {
-      store.set('users', 'abc123', {
+      store.set('users', '1', {
         firstname: 'Jimi',
         lastname: 'blabla',
         test: true
       })
-      var user = store.get('users', 'abc123')
+      var user = store.get('users', '1')
       assert.equal(user.firstname, 'Jimi')
       assert.equal(user.lastname, 'blabla')
       assert.equal(user.test, true)
+    })
+    it('delete', () => {
+      store.del('users', '1')
+      var user = store.get('1')
+      assert.equal(user, false)
+    })
+  })
+
+  describe('Full save', () => {
+    it('init', () => {
+      var init = store.init({
+        directory: 'documents',
+        documents: ['users'],
+        save: true
+      })
+      assert.equal(init, true)
+    })
+    it('get', () => {
+      store.set('users', '1', {
+        firstname: 'Jimi',
+        lastname: 'blabla',
+        test: true
+      })
+      store.set('users', '2', {
+        firstname: 'John',
+        lastname: 'Doe'
+      })
+      var user = store.get('users', '1')
+      assert.equal(user.firstname, 'Jimi')
+      assert.equal(user.lastname, 'blabla')
+      assert.equal(user.test, true)
+    })
+    it('delete', () => {
+      // store.del('users', '1')
+      var user1 = store.get('abc123')
+      assert.equal(user1, false)
+      var user2 = store.get('users', '2')
+      assert.equal(user2.firstname, 'John')
+      assert.equal(user2.lastname, 'Doe')
     })
   })
 })
