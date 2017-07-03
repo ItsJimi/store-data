fb63b7907f171b63b5ca784762fb42cbf3783d91
ItsJimi <jimi.maiquez@gmail.com>
2017-07-03 23:23:10 +0200

    (test) First init test with class

--- test/index.js
+++ test/index.js
@@ -1,38 +1,16 @@
-var assert = require('assert')
-var store = require('../lib')
+const assert = require('assert')
+const Store = require('../lib')
 
 /* global describe it */
 
 describe('store-data', () => {
   it('init', () => {
-    var init = store.init({
-      directory: 'documents',
-      documents: ['users'],
-      save: true
+    const users = new Store({
+      name: 'users',
+      directory: 'db',
+      save: false
     })
-    assert.equal(init, true)
-  })
-  it('get', () => {
-    store.set('users', '1', {
-      firstname: 'Jimi',
-      lastname: 'blabla',
-      test: true
-    })
-    store.set('users', '2', {
-      firstname: 'John',
-      lastname: 'Doe'
-    })
-    var user = store.get('users', '1')
-    assert.equal(user.firstname, 'Jimi')
-    assert.equal(user.lastname, 'blabla')
-    assert.equal(user.test, true)
-  })
-  it('delete', () => {
-    store.del('users', '1')
-    var user1 = store.get('abc123')
-    assert.equal(user1, false)
-    var user2 = store.get('users', '2')
-    assert.equal(user2.firstname, 'John')
-    assert.equal(user2.lastname, 'Doe')
+
+    console.log(users)
   })
 })
