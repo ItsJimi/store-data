2631255fcc4ad73a6e49ba4de723d6e9465c56bc
ItsJimi <jimi.maiquez@gmail.com>
2017-07-09 17:40:05 +0200

    Add persistent tests

--- test/index.js
+++ test/index.js
@@ -53,4 +53,55 @@ describe('store-data', () => {
       })
     })
   })
+
+  describe('Persistent (save = true)', () => {
+    let users
+
+    it('init', () => {
+      users = new Store({
+        save: true,
+        directory: 'storage',
+        name: 'users'
+      })
+    })
+
+    it('setSync', () => {
+      users.setSync('name', 'Jimi')
+    })
+
+    it('getSync', () => {
+      assert.deepEqual(users.getSync('name'), 'Jimi')
+    })
+
+    it('deleteSync', () => {
+      users.deleteSync('name')
+      assert.deepEqual(users.getSync('name'), undefined)
+    })
+
+    it('set', (done) => {
+      users.set('name', 'Jimi').then(() => {
+        done()
+      }).catch(err => {
+        done(err)
+      })
+    })
+
+    it('get', (done) => {
+      users.get('name').then(value => {
+        assert.deepEqual(value, 'Jimi')
+        done()
+      }).catch(err => {
+        done(err)
+      })
+    })
+
+    it('delete', (done) => {
+      users.delete('name').then(() => {
+        assert.deepEqual(users.getSync('name'), undefined)
+        done()
+      }).catch(err => {
+        done(err)
+      })
+    })
+  })
 })
